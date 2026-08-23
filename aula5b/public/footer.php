2c6ff0bfad10194f205b31d56d074bcf3f1becdc
profjosimar <josimar.ribeiro@prof.eteprc.faetec.rj.gov.br>
2026-08-23 12:26:14 -0300

    first commit

--- aula5b/public/footer.php
+++
@@ -1,5 +0,0 @@
-<?php if (basename($_SERVER['SCRIPT_NAME']) !== 'index.php'): ?> 
-    <div class="text-center mt-4"> 
-        <a href="index.php" class="btn btn-primary">Voltar para Início</a> 
-    </div> 
-<?php endif; ?> 
